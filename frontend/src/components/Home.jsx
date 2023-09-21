3dee795eaffce6db5da02d98fd38a38aa87b40d0
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-21 08:43:30 +0530

    added react type animaiton (Not working )

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import NotFound from "./NotFound";
 import Navbar from "./Navbar";
+import { TypeAnimation } from "react-type-animation";
 
 function Home() {
   const [data, setData] = useState("");
@@ -17,6 +18,28 @@ function Home() {
     return (
       <>
         <Navbar />
+        <div className="heading">
+          <h1>
+            <span>Love</span> with <span>Code</span>
+          </h1>
+        </div>
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            "We produce food for Mice",
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            "We produce food for Hamsters",
+            1000,
+            "We produce food for Guinea Pigs",
+            1000,
+            "We produce food for Chinchillas",
+            1000,
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: "2em", display: "inline-block" }}
+          repeat={Infinity}
+        /> 
       </>
     );
   } else {
