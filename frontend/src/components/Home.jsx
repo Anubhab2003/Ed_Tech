b85b98835b30a828ef0cfd9604ca2ba4b939d039
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-22 18:50:15 +0530

    worked on the main area of home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import NotFound from "./NotFound";
 import Navbar from "./Navbar";
 import { TypeAnimation } from "react-type-animation";
+import "../styles/home.scss";
+import gifSrc from "../assets/Programmer.gif";
 
 function Home() {
   const [data, setData] = useState("");
@@ -18,28 +20,34 @@ function Home() {
     return (
       <>
         <Navbar />
-        <div className="heading">
-          <h1>
-            <span>Love</span> with <span>Code</span>
-          </h1>
+        <div className="main">
+          <div>
+            <h1>
+              <span className="head-span">Love</span> with{" "}
+              <span className="head-span">Code</span>
+            </h1>
+
+            <TypeAnimation
+              className="animated-text"
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "We provide best videos from youtube",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "We provide tutorials",
+                1000,
+                "We provide notes",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "2em", display: "inline-block" }}
+              repeat={Infinity}
+            />
+            <a className="btn" href="#">Get started</a>
+          </div>
+
+          <img src={gifSrc} alt="gif" />
         </div>
-        <TypeAnimation
-          sequence={[
-            // Same substring at the start will only be typed out once, initially
-            "We produce food for Mice",
-            1000, // wait 1s before replacing "Mice" with "Hamsters"
-            "We produce food for Hamsters",
-            1000,
-            "We produce food for Guinea Pigs",
-            1000,
-            "We produce food for Chinchillas",
-            1000,
-          ]}
-          wrapper="span"
-          speed={50}
-          style={{ fontSize: "2em", display: "inline-block" }}
-          repeat={Infinity}
-        /> 
       </>
     );
   } else {
