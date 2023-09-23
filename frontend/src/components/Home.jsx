c63222b01c1dd7707765a30a659e5d9d5a5cceb7
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-23 21:49:52 +0530

    added another section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -51,11 +51,57 @@ function Home() {
           <img src={gifSrc} alt="gif" />
         </div>
         <main className="intro">
-          <h2>
-          Welcome to Dezire: Your Coding Companion
-          </h2>
-          <p>Are you eager to embark on an exciting coding journey? Look no further than Dezire – your ultimate destination for comprehensive notes, enlightening tutorials, and handpicked YouTube videos to fuel your passion for coding. Whether you are a seasoned developer seeking to refine your skills or a budding coder taking your first steps, Dezire is here to empower and inspire you.</p>
+          <h2>Welcome to Dezire: Your Coding Companion</h2>
+          <p>
+            Are you eager to embark on an exciting coding journey? Look no
+            further than Dezire – your ultimate destination for comprehensive
+            notes, enlightening tutorials, and handpicked YouTube videos to fuel
+            your passion for coding. Whether you are a seasoned developer
+            seeking to refine your skills or a budding coder taking your first
+            steps, Dezire is here to empower and inspire you.
+          </p>
         </main>
+        <section className="cards">
+          <div className="card">
+            <div className="card-heading">📘 In-Depth Notes:</div>
+            <div className="card-body">
+              Dive into a treasure trove of meticulously crafted notes covering
+              a wide range of programming languages, frameworks, and concepts.
+              Our expertly curated content simplifies complex topics, making
+              learning a breeze.
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-heading">🎥 Top-Quality Tutorials:</div>
+            <div className="card-body">
+            Explore our library of instructive tutorials that cater to all skill levels. From beginner-friendly primers to advanced techniques, you all find the guidance you need to thrive in the coding universe.
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-heading">📺 Best YouTube Videos:</div>
+            <div className="card-body">
+            We have scoured the vast realm of YouTube to bring you the finest coding videos. Discover insightful lectures, practical demonstrations, and engaging talks from coding luminaries. With our selection, you all never miss out on the latest trends and innovations.
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-heading">🌟 Community-Driven:</div>
+            <div className="card-body">
+            Join our vibrant community of coders, learners, and enthusiasts. Share your knowledge, seek advice, and collaborate on exciting projects. Dezire is more than a platform; it is a supportive ecosystem.
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-heading">🌐 Accessible Anywhere: </div>
+            <div className="card-body">
+            Access Dezire from your desktop, tablet, or mobile device. We are committed to ensuring your coding resources are always within reach, no matter where you are.
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-heading">💡 Stay Updated: </div>
+            <div className="card-body">
+            Stay at the forefront of coding with our regularly updated content. Our team is dedicated to keeping you informed about the latest developments in the tech world.
+            </div>
+          </div>
+        </section>
       </>
     );
   } else {
